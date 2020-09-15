bcf5f3d59ea44dcfce58c11760704ff475bb9bbc
Jessica Chan <jwinchan@gmail.com>
2020-09-15 15:49:39 -0700

    style and formatting changes

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,5 +1,5 @@
 def draw_letters
-  letters_hash = {A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 1, L: 4, M:2, N: 6, O: 8, P: 2, Q: 1, R: 6, S:4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1}
+  letters_hash = { A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 1, L: 4, M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1 }
   letters_array = []
 
   letters_hash.each_pair do |letter, frequency|
@@ -10,22 +10,19 @@ def draw_letters
   return letters_array.sample(10)
 end
 
-
 def uses_available_letters?(input, letters_in_hand)
   input_array = input.chars
   letters_copy = letters_in_hand.dup
-
   if input_array.length <= letters_in_hand.length
-   input_array.each do |letter|
-    if letters_copy.include?(letter)
-      letters_copy.delete(letter)
-    else
-      return false
+    input_array.each do |letter|
+      if letters_copy.include?(letter)
+        letters_copy.delete(letter)
+      else
+        return false
+      end
     end
-   end
-   return true
+    return true
   else
     return false
   end
 end
-
